df20339ea08f340931b22af8a640fcc504d2bd74
codestun <doerthy.k@gmail.com>
2023-09-25 15:38:59 +0200

    Add getAuthURL function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -41,3 +41,50 @@ module.exports.getAuthURL = async () => {
     }),
   };
 };
+
+module.exports.getAccessToken = async (event) => {
+  // Start of the function to get the access token using an authorization code.
+
+  // Extract and decode the authorization code from the provided event's path parameters.
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  // Create a new Promise to handle the asynchronous operation of fetching the access token.
+  return new Promise((resolve, reject) => {
+    /**
+     * Use the OAuth2 client to exchange the authorization code for an access token.
+     * This operation is asynchronous, so we provide a callback function to handle the response.
+     * The callback function receives two parameters: an error and a response.
+     * If there's an error, we reject the promise. If it's successful, we resolve the promise with the response.
+     */
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+  .then((results) => {
+    // If the promise is resolved (i.e., successful), this block is executed.
+
+    // Construct a success HTTP response with a status code of 200.
+    // The obtained access token (or other related data) is included in the response body as JSON.
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify(results),
+    };
+  })
+  .catch((error) => {
+    // If the promise is rejected (i.e., there was an error), this block is executed.
+
+    // Construct an error HTTP response with a status code of 500.
+    // The error message is included in the response body as JSON.
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  });
+};
