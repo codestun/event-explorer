10fd3dfb7e0df5730d42fd26a383709e6cd231de
codestun <doerthy.k@gmail.com>
2023-10-05 14:02:17 +0200

    Code & test first key feature of the app using TDD

---
+++ src/components/CitySearch.js
@@ -0,0 +1,50 @@
+// src/components/CitySearch.js
+
+import { useState, useCallback } from "react";
+
+const CitySearch = ({ allLocations }) => {
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  const handleInputChanged = useCallback((event) => {
+    const value = event.target.value;
+    const filteredLocations = allLocations ? allLocations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    }) : [];
+
+    setQuery(value);
+    setSuggestions(filteredLocations);
+  }, [allLocations]);
+
+  const handleItemClicked = (event) => {
+    const value = event.target.textContent;
+    setQuery(value);
+    setShowSuggestions(false); // to hide the list
+  };
+
+  return (
+    <div id="city-search">
+      <input
+        type="text"
+        className="city"
+        placeholder="Search for a city"
+        value={query}
+        onFocus={() => setShowSuggestions(true)}
+        onChange={handleInputChanged}
+      />
+      {showSuggestions ?
+        <ul className="suggestions">
+          {suggestions.map((suggestion) => {
+            return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
+          })}
+          <li key="see all cities">
+            <b>See all cities</b>
+          </li>
+        </ul>
+        : null}
+    </div>
+  )
+}
+
+export default CitySearch;
