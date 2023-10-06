cc8cd20a936751ae22188daa0db3d3ad290ea87d
codestun <doerthy.k@gmail.com>
2023-10-06 15:59:16 +0200

    Implement Event component and tests

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,14 +1,31 @@
 // src/components/Event.js
 
+import { useState } from 'react';
+
 const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
   return (
-    <li>
-      <h2>{event.summary}</h2> {/* Title */}
-      <p>{new Date(event.created).toString()}</p> {/* Start Time */}
-      <p>{event.location}</p> {/* Location */}
-      <button className="show-details-button">Show Details</button>
+    <li className="event-item">
+      <h3 className="event-title">{event.summary}</h3>
+      <p className="event-created">{event.created}</p>
+      <p className="event-location">{event.location}</p>
+      <button
+        className="event-details-btn"
+        onClick={() => {
+          setShowDetails(!showDetails);
+        }}
+      >
+        {showDetails ? 'Hide Details' : 'Show Details'}
+      </button>
+      {showDetails ? (
+        <div className="event-details">
+          <h4>Event Details</h4>
+          <p className="event-description">Description: {event.description}</p>
+          <p className="event-status">Event status: {event.status}</p>
+        </div>
+      ) : null}
     </li>
   );
-}
+};
 
 export default Event;
--- src/tests/Event.test.js
+++ src/tests/Event.test.js
@@ -1,33 +1,66 @@
 // src/__tests__/Event.test.js
-
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Event from '../components/Event';
-import mockData from '../mock-data';
+import { getEvents } from '../api';
 
-// Testing <Event /> component.
-describe('<Event /> component', () => {
-  // Use mock data for testing.
-  const allEvents = mockData;
+describe('<Event /> Component Tests', () => {
+  let EventComponent;
+  let allEvents;
 
-  // Ensure the component renders event details like title and time.
-  test('renders event details correctly', () => {
-    const event = allEvents[0];
-    const { queryByText } = render(<Event event={event} />);
-
-    // Check if the event title and start time are displayed.
-    expect(queryByText(event.summary)).toBeInTheDocument();
-    expect(queryByText(new Date(event.created).toString())).toBeInTheDocument();
+  // Fetch events using getEvents and select the first event before each test
+  beforeEach(async () => {
+    allEvents = await getEvents();
+    EventComponent = render(<Event event={allEvents[0]} />);
   });
 
-  // Ensure the event's location is displayed.
-  test('renders event location', () => {
-    const { queryByText } = render(<Event event={allEvents[0]} />);
-    expect(queryByText(allEvents[0].location)).toBeInTheDocument();
+  test('should render the event title', () => {
+    const titleElement = EventComponent.queryByText(allEvents[0].summary);
+    expect(titleElement).toBeInTheDocument();
   });
 
-  // Ensure the "Show Details" button is present.
-  test('renders event details button with the title (Show Details)', () => {
-    const { queryByText } = render(<Event event={allEvents[0]} />);
-    expect(queryByText("Show Details")).toBeInTheDocument();
+  test('should render the event time', () => {
+    const timeElement = EventComponent.queryByText(allEvents[0].created);
+    expect(timeElement).toBeInTheDocument();
+  });
+
+  test('should render the event location', () => {
+    const locationElement = EventComponent.queryByText(allEvents[0].location);
+    expect(locationElement).toBeInTheDocument();
+  });
+
+  test('should render the "Show Details" button', () => {
+    const showDetailsButton = EventComponent.queryByText('Show Details');
+    expect(showDetailsButton).toBeInTheDocument();
+  });
+
+  // Scenario 1: An event element is collapsed by default.
+  test('by default event details should be hidden', () => {
+    const eventDetailsContainer = EventComponent.container.firstChild;
+    const detailsElement = eventDetailsContainer.querySelector('.event-details');
+    expect(detailsElement).not.toBeInTheDocument();
+  });
+
+  // Scenario 2: User can expand an event to see details.
+  test('should show event details after clicking "Show Details"', async () => {
+    const showDetailsButton = EventComponent.queryByText('Show Details');
+    await userEvent.click(showDetailsButton);
+
+    const eventDetailsContainer = EventComponent.container.firstChild;
+    const detailsElement = eventDetailsContainer.querySelector('.event-details');
+    expect(detailsElement).toBeInTheDocument();
+  });
+
+  // Scenario 3: User can collapse an event to hide details.
+  test('should hide event details after clicking "Hide Details"', async () => {
+    const showDetailsButton = EventComponent.queryByText('Show Details');
+    await userEvent.click(showDetailsButton);
+
+    const hideDetailsButton = EventComponent.queryByText('Hide Details');
+    await userEvent.click(hideDetailsButton);
+
+    const eventDetailsContainer = EventComponent.container.firstChild;
+    const detailsElement = eventDetailsContainer.querySelector('.event-details');
+    expect(detailsElement).not.toBeInTheDocument();
   });
 });
