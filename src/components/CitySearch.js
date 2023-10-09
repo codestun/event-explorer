a4edf475ad6e18f314db4b54c45eaf75e348c703
codestun <doerthy.k@gmail.com>
2023-10-09 20:51:41 +0200

    Add integration testing for Feature 1 Scenario 3

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,26 +1,31 @@
 // src/components/CitySearch.js
 
-import { useState, useCallback } from "react";
+import { useState, useEffect } from "react";
 
-const CitySearch = ({ allLocations }) => {
+const CitySearch = ({ allLocations, setCurrentCity }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
 
-  const handleInputChanged = useCallback((event) => {
+  useEffect(() => {
+    setSuggestions(allLocations);
+  }, [`${allLocations}`]);
+
+
+  const handleInputChanged = (event) => {
     const value = event.target.value;
     const filteredLocations = allLocations ? allLocations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     }) : [];
-
     setQuery(value);
     setSuggestions(filteredLocations);
-  }, [allLocations]);
+  };
 
   const handleItemClicked = (event) => {
     const value = event.target.textContent;
     setQuery(value);
-    setShowSuggestions(false); // to hide the list
+    setShowSuggestions(false);
+    setCurrentCity(value);
   };
 
   return (
@@ -38,11 +43,12 @@ const CitySearch = ({ allLocations }) => {
           {suggestions.map((suggestion) => {
             return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
           })}
-          <li key="see all cities">
+          <li key='See all cities' onClick={handleItemClicked}>
             <b>See all cities</b>
           </li>
         </ul>
-        : null}
+        : null
+      }
     </div>
   )
 }
