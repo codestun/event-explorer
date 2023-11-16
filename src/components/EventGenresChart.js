6c4354482de1f498e63abbb6a6da1b6da78c5071
codestun <doerthy.k@gmail.com>
2023-11-16 14:45:15 +0100

    Expand EventGenresChart with data processing

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react';
 import { PieChart, Pie, ResponsiveContainer } from 'recharts';
 
 const EventGenresChart = ({ events }) => {
+  const [data, setData] = useState([]);
+
+  // Genres that will make up the pie slices
+  const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
+
+  // Function to calculate data for each genre
+  const getData = () => {
+    const data = genres.map((genre) => {
+      const filteredEvents = events.filter((event) =>
+        event.summary.includes(genre)
+      );
+      return { name: genre, value: filteredEvents.length };
+    });
+    return data;
+  };
+
+  // Effect to update chart data when events change
+  useEffect(() => {
+    setData(getData());
+  }, [events]);
+
   return (
     <ResponsiveContainer width="99%" height={400}>
       <PieChart>
-        <Pie data={[]} dataKey="value" />
+        <Pie data={data} dataKey="value" />
       </PieChart>
     </ResponsiveContainer>
   );
